a5a9bf255bb27faafb5085a918695a0b42a06805
CameronHardy9 <camhardy99@gmail.com>
2022-08-04 15:55:54 -0400

    Renamed project

--- bin/index.js
+++ bin/index.js
@@ -3,30 +3,56 @@
 const readlineSync = require("readline-sync");
 const exif = require("exiftool");
 const fs = require("fs");
-const path = require('path');
-const process = require('process');
-const cliProgress = require('cli-progress');
+const path = require("path");
+const process = require("process");
+const cliProgress = require("cli-progress");
 
-const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-let progress = 0
+const progressBar = new cliProgress.SingleBar(
+	{},
+	cliProgress.Presets.shades_classic
+);
+let progress = 0;
+let filesArr = [];
 
 const input = process.argv[2];
 const dir = path.resolve(process.cwd(), input);
 
-
 fs.readdir(dir, (err, files) => {
-    if(err){
-        console.log(err);
-    } else{
-        console.log(files.length);
+	if (err) {
+		console.log(err);
+	} else {
+		progressBar.start(files.length, 0);
+        filesArr = files;
+	}
+});
+
+
+fs.readFile(filesArr[progress], (err, data) => {
+    if (err){
+            throw err;
+        }
+    else {
+        exif.metadata(data, ['-creationDate', '-createDate'], (err, metadata) => {
+            if (err) {
+                    throw err;
+                }
+            else {
+                    console.log(metadata);
+                    progress += 1;
+                    progressBar.update(progress);
+                }
+        });
     }
-  });
+});
+
+
+
 
 // if (answer === "yes") {
 // 	fs.readFile("IMG_8632 (1).mov", (err, data) => {
 // 		if (err){
 //             throw err;
-//         } 
+//         }
 // 		else {
 // 			exif.metadata(data, ['-creationDate', '-createDate'], (err, metadata) => {
 // 				if (err) {
