60fa10bf62f23f6378590f70fc284f4a08800064
CameronHardy9 <camhardy99@gmail.com>
2022-08-19 14:38:26 -0400

    Added CLI menu prompts and file 'Created' date rewrite option

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
 
-const readlineSync = require("readline-sync");
 const fs = require("fs");
 const path = require("path");
 const process = require("process");
 const cliProgress = require("cli-progress");
 const exiftool = require("exiftool-vendored").exiftool;
+const { lutimesSync } = require('utimes');
+const dayjs = require('dayjs');
+const prompts = require('prompts');
 
 const progressBar = new cliProgress.SingleBar(
 	{},
@@ -23,77 +25,132 @@ if (files.length === 0) {
     return 0;
 }
 
-const continueProgram = readlineSync.question(`\n${files.length} ${files.length > 1 ? "files" : "file"} will be impacted from the ${dir} filepath.\nAre you sure you want to continue? [yes/no] `);
+const questions = [
+    {
+        type: 'select',
+        name: 'execute',
+        message: `Are you sure you want to continue?`,
+        choices: [
+            {
+                title: 'Continue',
+                value: true
+            },
+            {
+                title: 'Quit',
+                value: false
+            }
+        ]
+    },
+    {
+        type: prev => prev == true ? 'select' : null,
+        name: 'typeOfSort',
+        message: 'Which operation would you like to execute?',
+        choices: [
+            {
+                title: 'Organize files into a nested Year>Month folder structure',
+                value: 'folder'
+            },
+            {
+                title: 'Rewrite file "Created Date" to match original creation date in metadata',
+                value: 'rewrite'
+            },
+            {
+                title: 'Both',
+                value: 'both'
+            }
+        ]
+    }
+];
+
+(async () => {
+    console.log(`\n${files.length} ${files.length > 1 ? "files" : "file"} will be impacted from the ${dir} filepath.`)
+    const { execute, typeOfSort } = await prompts(questions);
 
 
-if(continueProgram.toLowerCase() === 'yes'){
-    progressBar.start(files.length, 0);
+    if(execute){
+        progressBar.start(files.length, 0);
+    
+        const promises = files.map((file, index) => {
+            const ext = path.extname(file);
+            let skipFile = false;
+            let tag;
+    
+            switch(true){
+                case ['.mov', '.MOV'].includes(ext):
+                    tag = 'CreationDate';
+                    break;
+                case ['.jpg', '.jpeg', '.HEIC', '.heic', '.png'].includes(ext):
+                    tag = 'DateTimeOriginal';
+                    break;
+                default:
+                    skippedFiles.push(file);
+                    skipFile = true;
+                    progressBar.update(files.length - (files.length - (index + 1)));
+            };
+    
+            if (!skipFile) {
+                return exiftool.read(path.resolve(dir, file), ["-stay_open"]).then(({[tag]: time}) => {
+                    
+                    if(['both', 'rewrite'].includes(typeOfSort)){
+                        const dateCreated = +dayjs(`${time.month}-${time.day}-${time.year} ${time.hour}:${time.minute}:${time.second}`, 'M-D-YYYY H:m:s');
 
-    const promises = files.map((file) => {
-        const ext = path.extname(file);
-        let skipFile = false;
-        let tag;
+                        lutimesSync(path.resolve(dir, file), {
+                            btime: dateCreated
+                        });
 
-        switch(true){
-            case ['.mov', '.MOV'].includes(ext):
-                tag = 'CreationDate';
-                break;
-            case ['.jpg', '.jpeg', '.HEIC', '.heic', '.png'].includes(ext):
-                tag = 'DateTimeOriginal';
-                break;
-            default:
-                skippedFiles.push(file);
-                skipFile = true;
-                progressBar.increment();
-        };
-
-        if (!skipFile) {
-            return exiftool.read(path.resolve(dir, file), ["-stay_open"]).then((metadata) => {
-                const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-                const year = `${metadata[tag].year}`;
-                const month = months[metadata[tag].month - 1];
-
-                const targetPathExists = fs.existsSync(dir);
-                const yearDirExists = fs.existsSync(path.join(dir, year));
-                const monthDirExists = fs.existsSync(path.join(dir, year, month));
-
-                if(targetPathExists){
-                    if(yearDirExists){
-                        if(monthDirExists) {
-                            fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
-                        } else {
-                            fs.mkdirSync(path.join(dir, year, month));
-                            fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
-                        }
-                    } else {
-                        fs.mkdirSync(path.join(dir, year));
-                        fs.mkdirSync(path.join(dir, year, month));
-                        fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
+                        progressBar.update(files.length - (files.length - (index + 1)));
                     }
-                    progressBar.increment();
-                } else {
-                    progressBar.stop();
-                    console.log('ERROR: Your target file path has changed\nprogram terminated');
-                    return 1;
-                }
-            })
-        }
-    });
 
-    Promise.all(promises).finally(() => {
-        exiftool.end();
-        progressBar.stop();
-
-        console.log("\nSorting complete!");
+                    if(['both', 'folder'].includes(typeOfSort)){
+                        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                        const year = `${time.year}`;
+                        const month = months[time.month - 1];
         
-        if(skippedFiles.length > 0) {
-            const amountSkippedPlural = skippedFiles.length > 1;
-            console.log(`${skippedFiles.length} ${amountSkippedPlural ? 'files' : 'file'} ${amountSkippedPlural ? 'were' : 'was'} skipped and ${amountSkippedPlural ? 'remain' : 'remains'} in ${amountSkippedPlural ? 'their' : "it's"} original location.`);
-        }
+                        const targetPathExists = fs.existsSync(dir);
+                        const yearDirExists = fs.existsSync(path.join(dir, year));
+                        const monthDirExists = fs.existsSync(path.join(dir, year, month));
+        
+                        if(targetPathExists){
+                            if(yearDirExists){
+                                if(monthDirExists) {
+                                    fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
+                                } else {
+                                    fs.mkdirSync(path.join(dir, year, month));
+                                    fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
+                                }
+                            } else {
+                                fs.mkdirSync(path.join(dir, year));
+                                fs.mkdirSync(path.join(dir, year, month));
+                                fs.renameSync(path.join(dir, file), path.join(dir, year, month, file));
+                            }
+                            progressBar.update(files.length - (files.length - (index + 1)));
+                        } else {
+                            progressBar.stop();
+                            console.log('ERROR: Your target file path has changed\nprogram terminated');
+                            return 1;
+                        }
+                    }
+                })
+            }
+        });
+    
+        Promise.all(promises).finally(() => {
+            exiftool.end();
+            progressBar.stop();
+    
+            console.log("\nTask complete!");
+            
+            if(skippedFiles.length > 0) {
+                const amountSkippedPlural = skippedFiles.length > 1;
+                console.log(`${skippedFiles.length} ${amountSkippedPlural ? 'files' : 'file'} ${amountSkippedPlural ? 'were' : 'was'} skipped and ${amountSkippedPlural ? 'remain' : 'remains'} in ${amountSkippedPlural ? 'their' : "it's"} original state.`);
+            }
+            return 0;
+        });
+    
+    } else {
+        console.log('Program terminated\n');
         return 0;
-    });
+    }
+})();
 
-} else {
-    console.log('Program terminated');
-    return 0;
-}
+
